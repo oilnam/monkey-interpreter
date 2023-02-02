eefb732b8bcc35c5ead49bd16655c1c8c37ccea5
manlio <manlio.poltronieri@gmail.com>
2023-02-02 16:06:25 +0000

    comment out old test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -102,32 +102,32 @@ func TestNewReturnStatements(t *testing.T) {
 	}
 }
 
-func TestOldReturnStatements(t *testing.T) {
-	input := `
-	return 5;
-	return 10;
-	return 993322;`
-
-	l := lexer.New(input)
-	p := New(l)
-
-	program := p.ParseProgram()
-	checkParserErrors(t, p)
-
-	assert.Len(t, program.Statements, 3)
-
-	for _, stmt := range program.Statements {
-		returnStmt, ok := stmt.(*ast.ReturnStatement)
-		if !ok {
-			t.Errorf("stmt not *ast.returnStatement. got=%T", stmt)
-			continue
-		}
-		if returnStmt.TokenLiteral() != "return" {
-			t.Errorf("returnStmt.TokenLiteral not 'return', got %q",
-				returnStmt.TokenLiteral())
-		}
-	}
-}
+//func TestOldReturnStatements(t *testing.T) {
+//	input := `
+//	return 5;
+//	return 10;
+//	return 993322;`
+//
+//	l := lexer.New(input)
+//	p := New(l)
+//
+//	program := p.ParseProgram()
+//	checkParserErrors(t, p)
+//
+//	assert.Len(t, program.Statements, 3)
+//
+//	for _, stmt := range program.Statements {
+//		returnStmt, ok := stmt.(*ast.ReturnStatement)
+//		if !ok {
+//			t.Errorf("stmt not *ast.returnStatement. got=%T", stmt)
+//			continue
+//		}
+//		if returnStmt.TokenLiteral() != "return" {
+//			t.Errorf("returnStmt.TokenLiteral not 'return', got %q",
+//				returnStmt.TokenLiteral())
+//		}
+//	}
+//}
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	if s.TokenLiteral() != "let" {
