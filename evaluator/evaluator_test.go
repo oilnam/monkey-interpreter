24cc5ac4c7d89c51f843da308a02d579a656e2d4
manlio <manlio.poltronieri@gmail.com>
2023-02-13 18:56:09 +0000

    Page 151

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -266,6 +266,16 @@ func TestFunctionApplication(t *testing.T) {
 	}
 }
 
+func TestClosures(t *testing.T) {
+	input := `
+   let newAdder = fn(x) {
+     fn(y) { x + y };
+};
+   let addTwo = newAdder(2);
+   addTwo(2);`
+	testIntegerObject(t, testEval(input), 4)
+}
+
 // helpers
 
 func testEval(input string) object.Object {
